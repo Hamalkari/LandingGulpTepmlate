90e7ed12fcdc4f4d3d8177c72e1a9ce5baa0ef38
Halmakari <kosha.1997@bk.ru>
2019-06-22 23:18:48 +0300

    Добавил сжатие svg

--- gulp-tasks/sprites.js
+++ gulp-tasks/sprites.js
@@ -3,12 +3,18 @@
 import {paths} from '../gulpfile.babel';
 import gulp from 'gulp';
 import svg from 'gulp-svg-sprite';
+import svgmin from 'gulp-svgmin';
 import debug from 'gulp-debug';
 import browswerSync from 'browser-sync';
 
 
 gulp.task('sprites',() => {
     return gulp.src(paths.sprites.src)
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
         .pipe(svg({
             shape: {
                 dest: 'intermediate-svg'
